2dc5656669095266b64fbaa6ba15fe56245c4b92
KenObata <kenichiderumo@gmail.com>
2025-08-17 19:26:28 -0400

    moved create sesion to util file.

--- src/spark_deduplication.py
+++ src/spark_deduplication.py
@@ -16,28 +16,7 @@ from typing import List, Tuple
 from collections import defaultdict
 import hashlib
 
-# from src.spark_utils import create_deduplication_spark_session
-
-def create_deduplication_spark_session() -> SparkSession:
-    """Create Spark session optimized for deduplication"""
-    
-    spark = SparkSession.builder \
-        .appName("WebScaleDeduplication") \
-        .config("spark.sql.adaptive.enabled", "true") \
-        .config("spark.sql.adaptive.coalescePartitions.enabled", "true") \
-        .config("spark.sql.adaptive.skewJoin.enabled", "true") \
-        .config("spark.executor.memory", "8g") \
-        .config("spark.executor.cores", "4") \
-        .config("spark.driver.memory", "4g") \
-        .config("spark.sql.shuffle.partitions", "200") \
-        .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer") \
-        .config("spark.sql.execution.arrow.pyspark.enabled", "true") \
-        .getOrCreate()
-    
-    # Set log level to reduce verbosity
-    spark.sparkContext.setLogLevel("WARN")
-    
-    return spark
+from src.spark_utils import create_deduplication_spark_session
 
 def create_minhash_udf(num_hashes: int = 128, k: int = 9):
     """
